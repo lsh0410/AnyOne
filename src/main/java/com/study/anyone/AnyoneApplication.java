432a95fe93046739dda5e781dfbd08f4f377205c
tngks0410 <tngks0410@gmail.com>
2023-09-19 14:01:20 +0900

    first commit

---
+++ src/main/java/com/study/anyone/AnyoneApplication.java
@@ -0,0 +1,13 @@
+package com.study.anyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyoneApplication.class, args);
+	}
+
+}
